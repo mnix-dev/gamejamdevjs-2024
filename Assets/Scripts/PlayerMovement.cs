f7b156890cf1433297f1408b1b7a25963d38b985
Mikey Nichols <mnix@journeytocode.io>
2024-04-21 20:00:35 -0400

    adds pseudocode for animation on movent

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent (typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Necessary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    // Variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;
+
+
+
+    private void Start()
     {
-        
+        // defines the game objects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // handles the input for physics
+    private void Update()
     {
-        
+        // check if the player has input movement
+    }
+    // handles running the physics
+    private void FixedUpdate()
+    {
+        // move the player's character left and right
     }
 }
