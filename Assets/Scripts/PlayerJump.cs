77e2f633630472d0a5c5da4c35dc3b1943ba3630
Mikey Nichols <mnix@journeytocode.io>
2024-04-23 10:28:41 -0400

    adds extra jump time if space bar is held

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -7,6 +7,10 @@ public class PlayerJump : MonoBehaviour
     // force, apply force
     [Header("Jump Details")]
     public float jumpforce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
+
     [Header("Ground Details")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float radOCircle;
@@ -16,20 +20,39 @@ public class PlayerJump : MonoBehaviour
     [Header("Components")]  
     private Rigidbody2D rb;
 
-
     private void Start()
     {
+        jumpTimeCounter = jumpTime;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
-    {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround); 
-         if (Input.GetButtonDown("Jump") && grounded|| (Input.GetAxis("Vertical") > 0 && grounded))
-            
+    {   // what it means to be grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
+        if ( grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+        {
+            // if the jump button is pressed down
+        }
+        if (Input.GetButtonDown("Jump") && grounded )
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            stoppedJumping = false;
         }
+           // if jump button is held down
+        if (Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0)) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        // if jump button is released
+        if (Input.GetButtonUp("Jump") ) {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        } 
+
     }
 
     private void OnDrawGizmos()
