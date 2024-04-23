ccc2f1e0d3d517af066b7ea5d3590bfc6f761150
Mikey Nichols <mnix@journeytocode.io>
2024-04-22 20:10:58 -0400

    adds long jump function

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -5,22 +5,61 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     // force, apply force
+    [Header("Jump Details")]
+    public float jumpForce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
 
-    public float jumpforce;
+    [Header("Ground Details")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float radOCircle;
+    [SerializeField] private LayerMask whatIsGround;
     public bool grounded;
+
+    [Header("Components")]
     private Rigidbody2D rb;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        // What it means to be grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
+
+        if (grounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            jumpTimeCounter = jumpTime;
+        }
+
+        // Use jump key
+        if (Input.GetButtonDown("Jump") && grounded || Input.GetButtonDown("Up") && grounded)
+        {
+            // jump!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
+        }
+        // if we hold jump button
+        if (Input.GetButton("Jump") && !stoppedJumping || Input.GetButton("Up") && !stoppedJumping)
+        {
+            // jump!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        // if we relese the jump button
+        if (Input.GetButtonUp("Jump") || Input.GetButtonUp("Up"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
         }
     }
-
-}
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, radOCircle);
+    }
+     
